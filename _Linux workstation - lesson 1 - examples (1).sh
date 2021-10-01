38c451f3e709f866252e2c36c67b03b003b95dc0
Vitaly <88037272+vit050587@users.noreply.github.com>
2021-10-01 13:59:15 +0300

    Add files via upload

---
+++ _Linux workstation - lesson 1 - examples (1).sh
@@ -0,0 +1,20 @@
+# вывести имя ОС
+uname
+
+# вывести имя ОС с подробностями
+uname -a
+
+# запросить имя хоста (машины)
+hostnamectl
+
+# вывести инфо о дистрибутиве
+lsb_release -a
+
+# обновить список репозиториев
+sudo apt update
+
+# установить пакет mc
+sudo apt install mc
+
+# запустить Midnight Commander
+mc
